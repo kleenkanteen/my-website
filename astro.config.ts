10308f654389de56b82c3efe5ba5f883a1ec7500
Sabih S <antpalmolive@gmail.com>
2025-10-14 09:42:25 -0400

    analytics: add vercel

--- astro.config.ts
+++ astro.config.ts
@@ -4,11 +4,17 @@ import react from "@astrojs/react";
 import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
+import vercel from '@astrojs/vercel';
 import { SITE } from "./src/config";
 
 // https://astro.build/config
 export default defineConfig({
   site: SITE.website,
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
   integrations: [
     tailwind({
       applyBaseStyles: false,
